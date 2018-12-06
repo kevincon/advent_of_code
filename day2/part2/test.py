faced59f30c20b0f513cc8a841712ecd827a9991
Kevin Conley <kevincon@users.noreply.github.com>
2018-12-06 07:33:02 +0000

    Add missing test coverage for day2 part2 solution

--- day2/part2/test.py
+++ day2/part2/test.py
@@ -3,10 +3,12 @@ import itertools
 import operator
 import os
 import pathlib
+import pytest
 import typing
 
 
 def find_id_pair_that_differs_by_one_character(ids: typing.Iterable[str]) -> typing.Optional[typing.Tuple[str, str]]:
+    """https://adventofcode.com/2018/day/2#part2"""
     combinations: typing.List[typing.Tuple[str, str]] = itertools.combinations(ids, 2)
     for combination in combinations:
         letter_pairs = zip(*combination)
@@ -28,9 +30,10 @@ def test_find_id_pair_that_differs_by_one_character():
         'wvxyz',
     ]) == ('fghij', 'fguij')
 
+    assert find_id_pair_that_differs_by_one_character([]) is None
+
 
 def get_answer(ids: typing.Iterable[str]) -> str:
-    """https://adventofcode.com/2018/day/2#part2"""
     answer_pair = find_id_pair_that_differs_by_one_character(ids)
     if not answer_pair:
         raise Exception('No id pair that differs by only one character')
@@ -56,3 +59,6 @@ def test_answer():
     module_dir = os.path.dirname(os.path.realpath(__file__))
     with pathlib.Path(module_dir, 'input.txt').open() as f:
         assert get_answer(f.read().splitlines()) == 'cvgywxqubnuaefmsljdrpfzyi'
+
+    with pytest.raises(Exception):
+        get_answer([])
