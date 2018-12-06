c5792f7b2aace3d85b16603c95280af91f4802f7
Kevin Conley <kevincon@users.noreply.github.com>
2018-12-06 07:33:16 +0000

    Simplify day3 solutions

--- day3/test.py
+++ day3/test.py
@@ -1,4 +1,3 @@
-import collections
 import itertools
 import os
 import pathlib
@@ -7,61 +6,16 @@ import re
 import typing
 
 
-class LineSegment(typing.NamedTuple):
-    start: int
-    length: int
-
-    @property
-    def end(self) -> int:
-        return self.start + self.length
-
-    def intersect(self, other: 'LineSegment') -> typing.Optional['LineSegment']:
-        first, second = sorted([self, other], key=lambda ls: ls.start)
-        if second.start >= first.end:
-            return None
-        return LineSegment(start=second.start, length=(min(first.end, second.end) - second.start))
-
-
-def test_line_segment_intersect():
-    assert LineSegment(1, 4).intersect(LineSegment(2, 5)) == LineSegment(2, 3)
-    assert LineSegment(3, 5).intersect(LineSegment(1, 4)) == LineSegment(3, 2)
-    assert LineSegment(1, 4).intersect(LineSegment(2, 2)) == LineSegment(2, 2)
-    assert LineSegment(1, 2).intersect(LineSegment(4, 2)) is None
-    assert LineSegment(2, 2).intersect(LineSegment(4, 2)) is None
-
-
 class Rectangle(typing.NamedTuple):
     x: int
     y: int
     width: int
     height: int
 
-    def x_dimension_line_segment(self) -> LineSegment:
-        return LineSegment(start=self.x, length=self.width)
-
-    def y_dimension_line_segment(self) -> LineSegment:
-        return LineSegment(start=self.y, length=self.height)
-
-    def intersect(self, other: 'Rectangle') -> typing.Optional['Rectangle']:
-        x_dimension_intersection = self.x_dimension_line_segment().intersect(other.x_dimension_line_segment())
-        y_dimension_intersection = self.y_dimension_line_segment().intersect(other.y_dimension_line_segment())
-
-        if None in [x_dimension_intersection, y_dimension_intersection]:
-            return None
-
-        return Rectangle(x=x_dimension_intersection.start, y=y_dimension_intersection.start,
-                         width=x_dimension_intersection.length, height=y_dimension_intersection.length)
-
     def coordinate_iterator(self) -> typing.Iterator[typing.Tuple[int, int]]:
         for x in range(self.x, self.x + self.width):
             for y in range(self.y, self.y + self.height):
                 yield x, y
-
-
-def test_rectangle_intersect():
-    assert Rectangle(0, 0, 100, 100).intersect(Rectangle(25, 25, 50, 50)) == Rectangle(25, 25, 50, 50)
-    assert Rectangle(10, 10, 10, 10).intersect(Rectangle(15, 10, 10, 10)) == Rectangle(15, 10, 5, 10)
-    assert Rectangle(10, 10, 10, 10).intersect(Rectangle(20, 10, 10, 10)) is None
 
 
 class Claim(typing.NamedTuple):
@@ -86,51 +40,34 @@ def test_claim_from_string():
     assert claim_from_string('#4321 @ 45,83: 24x13') == Claim(id=4321, rect=Rectangle(45, 83, 24, 13))
 
 
-class Bitmap:
+class CounterMap:
     def __init__(self, width: int, height: int):
-        self._map = [[False for _ in range(height)] for _ in range(width)]
-
-    @property
-    def width(self):
-        return len(self._map)
-
-    @property
-    def height(self):
-        return len(self._map[0])
-
-    def _check_if_coordinate_in_bounds(self, x, y):
-        if x < 0 or x > self.width or y < 0 or y > self.height:
-            raise Exception(f'({x}, {y}) is out of bitmap bounds ({self.width}, {self.height}')
+        self._map = [[0 for _ in range(height)] for _ in range(width)]
 
     def update(self, rect: Rectangle) -> None:
         for x, y in rect.coordinate_iterator():
-            self._check_if_coordinate_in_bounds(x, y)
-            self._map[x][y] = True
+            self._map[x][y] += 1
 
-    def total_area(self) -> int:
+    def common_area(self) -> int:
         flattened_map = list(itertools.chain(*self._map))
-        counter = collections.Counter(flattened_map)
-        return counter[True]
+        return len(list(filter(lambda x: x > 1, flattened_map)))
 
     def does_cover_rect(self, rect: Rectangle) -> bool:
         for x, y in rect.coordinate_iterator():
-            self._check_if_coordinate_in_bounds(x, y)
-            if self._map[x][y]:
+            if self._map[x][y] > 1:
                 return True
         return False
 
 
-def create_bitmap_for_claims(claims: typing.List[Claim]) -> Bitmap:
-    result = Bitmap(width=1000, height=1000)
-    for claim1, claim2 in itertools.combinations(claims, 2):
-        intersection = claim1.rect.intersect(claim2.rect)
-        if intersection is not None:
-            result.update(intersection)
+def create_counter_map_for_claims(claims: typing.List[Claim]) -> CounterMap:
+    result = CounterMap(width=1000, height=1000)
+    for claim in claims:
+        result.update(claim.rect)
     return result
 
 
 def calculate_area_common_to_two_or_more_claims(claims: typing.List[Claim]) -> int:
-    return create_bitmap_for_claims(claims).total_area()
+    return create_counter_map_for_claims(claims).common_area()
 
 
 def claims_from_claim_strings(claim_strings: typing.List[str]) -> typing.List[Claim]:
@@ -138,6 +75,7 @@ def claims_from_claim_strings(claim_strings: typing.List[str]) -> typing.List[Cl
 
 
 def calculate_area_common_to_two_or_more_claim_strings(claim_strings: typing.List[str]) -> int:
+    """https://adventofcode.com/2018/day/3"""
     return calculate_area_common_to_two_or_more_claims(claims_from_claim_strings(claim_strings))
 
 
@@ -162,12 +100,17 @@ def test_part1_answer(input_file_lines):
 
 
 def get_first_uncovered_claim_in_claim_strings(claim_strings: typing.List[str]) -> typing.Optional[Claim]:
+    """https://adventofcode.com/2018/day/3#part2"""
     claims = claims_from_claim_strings(claim_strings)
-    bitmap = create_bitmap_for_claims(claims)
+    bitmap = create_counter_map_for_claims(claims)
     for claim in claims:
         if not bitmap.does_cover_rect(claim.rect):
             return claim
     return None
+
+
+def test_get_first_uncovered_claim_in_claim_strings():
+    assert get_first_uncovered_claim_in_claim_strings([]) is None
 
 
 def test_part2_answer(input_file_lines):
